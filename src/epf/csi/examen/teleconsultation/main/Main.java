b8055619a0aa609df63502c12af89c990bb2f55f
Brownshamallow <thiombanendoumbe02@gmail.com>
2025-05-03 17:29:15 +0000

    classe main

---
+++ src/epf/csi/examen/teleconsultation/main/Main.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package epf.csi.examen.teleconsultation.main;
+
+/**
+ * 
+ */
+public class Main {
+
+	/**
+	 * 
+	 */
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
